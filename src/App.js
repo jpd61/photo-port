6ccde81f73c8ce7f6a8c8e061c2a5a917ff324d2
jpd61 <jpd@dewoodyoil.com>
2020-08-01 11:19:01 -0500

    react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
-import Nav from './components/Nav';
 import About from './components/About';
+import Nav from './components/Nav'
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 function App() {
+  // lifting from Nav
   const [categories] = useState([
     {
       name: 'commercial',
@@ -16,18 +18,27 @@ function App() {
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
+  // contact form is not initially shown, gallery is 
+  const [contactSelected, setContactSelected] = useState(false);
+
   return (
     <div>
       <Nav
-        categories={categories}
-        setCurrentCategory={setCurrentCategory}
-        currentCategory={currentCategory}
+      categories={categories}
+      setCurrentCategory={setCurrentCategory}
+      currentCategory={currentCategory}
+      contactSelected={contactSelected}
+      setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        <div>
-          <Gallery></Gallery>
-          <About></About>
-        </div>
+        {!contactSelected ? (
+        <>
+        <Gallery currentCategory={currentCategory}></Gallery>
+        <About></About>
+        </>
+        ) : (
+        <ContactForm></ContactForm>
+        )}
       </main>
     </div>
   );
